51bbcd7a3ec2463dbc0232cf488df9ee001e44cd
min33sky <min33sky@naver.com>
2018-10-07 19:31:24 +0900

    add viewerContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react';
 import { ViewTemplate } from './ViewTemplate';
 import { SpaceNavigator } from './SpaceNavigator';
-import Viewer from './Viewer';
 
-import * as api from 'lib/api';
+import ViewerContainer from 'containers/ViewerContainer';
 
 class App extends Component {
-  1;
-  componentDidMount() {
-    this.getApod();
-  }
-
-  getApod = async date => {
-    try {
-      const response = await api.getAPOD(date);
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   render() {
     return (
       <ViewTemplate
-        viewer={
-          <Viewer
-            url="https://www.youtube.com/embed/uj3Lq7Gu94Y?rel=0"
-            mediaType="video"
-          />
-        }
+        viewer={<ViewerContainer />}
         spaceNavigator={<SpaceNavigator />}
       />
     );
--- src/containers/ViewerContainer.js
+++ src/containers/ViewerContainer.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as apodActions from 'store/modules/apod';
+import Viewer from 'components/Viewer';
 
 class ViewerContainer extends Component {
+  req = null;
+
+  getApod = async () => {
+    const { ApodActions, loading, date } = this.props;
+    loading && this.req.cancel(); // 로딩중이라면 기존의 요청 취소
+
+    try {
+      // this.req에 Promise 담기
+      this.req = ApodActions.getApod(date || '');
+      await this.req;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  componentDidMount() {
+    this.getApod();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // date가 변경되면 요청
+    if (this.props.date !== prevProps.date) {
+      this.getApod();
+    }
+  }
+
   render() {
-    return <div />;
+    const { date, url, mediaType, loading } = this.props;
+
+    return (
+      <Viewer date={date} url={url} mediaType={mediaType} loading={loading} />
+    );
   }
 }
 
